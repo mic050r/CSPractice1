9e65e4712815098c28a47154a4e8c608260888ca
mic050r <s2216@e-mirim.hs.kr>
2024-07-01 14:55:20 +0900

    Feat (#5) 22-9. 값 복사와 참조복사

--- Program.cs
+++ Program.cs
@@ -67,7 +67,14 @@ namespace CSPractice1
 
     internal class Program
     {
-
+        class Test
+        {
+            public int value = 10;
+        }
+        static void Change(Test t)
+        {
+            t.value = 20;
+        }
         class ThirdClass
         {
 
@@ -262,6 +269,13 @@ namespace CSPractice1
             b.width = 100;
             b.Height = 100;
             // b.Height = -100; // 예외발생
+
+            // #5 22-9. 값 복사와 참조복사
+
+            Test test = new Test();
+            test.value = 100;
+            Change(test);
+            Console.WriteLine(test.value); // 20
         }
     }
 }
