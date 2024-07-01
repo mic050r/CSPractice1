4dfef544c31daa0300daf4a24407fe0ae6ced799
mic050r <s2216@e-mirim.hs.kr>
2024-07-01 15:00:15 +0900

    Feat (#6) 23-4. is 키워드(+ as 키워드)

---
+++ Animal.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSPractice1
+{
+    internal class Animal
+    {
+        public int Age;
+        public Animal()
+        {
+
+        }
+        public void Eat()
+        {
+            Console.WriteLine("¸Ô´Â´Ù");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine("ÀÜ´Ù");
+        }
+
+    }
+}
